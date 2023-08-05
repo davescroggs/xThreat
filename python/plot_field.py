6b77692dd5ffa661bc4868e81989b6ca1e66fa48
Dave Scroggs <davidscroggs7@gmail.com>
2023-08-05 17:42:20 +1000

    Added straight goal line to playing area

--- python/plot_field.py
+++ python/plot_field.py
@@ -3,7 +3,7 @@ from matplotlib import pyplot as plt
 import matplotlib.patches as mpatches
 from math import pi
 
-def generate_afl_oval(v, plot_grid = True):
+def generate_afl_oval(v):
 
     venueDims = {'MCG': {'venueLength': 160, 'venueWidth': 141},
                 'Marvel Stadium': {'venueLength': 160, 'venueWidth': 129},
@@ -23,46 +23,54 @@ def generate_afl_oval(v, plot_grid = True):
                 'TIO Stadium': {'venueLength': 175, 'venueWidth': 135},
                 'TIO Traeger Park': {'venueLength': 168, 'venueWidth': 132}}
 
-    
     x_lim = venueDims[v]['venueLength'] / 2
     y_lim = venueDims[v]['venueWidth'] / 2
-    
-    # Optionally plot grid
-    if plot_grid:
-        plt.grid(color='lightgray',linestyle='--')
 
-    # Plot oval shape
-    t = np.linspace(0, 2*pi, 100)
-    plt.plot(x_lim*np.cos(t) ,y_lim*np.sin(t), 'k-')
-    
+    plt.grid(color='lightgray',linestyle='--')
+    t = np.linspace(0, 2*pi, 500)
+
+    x_sq = np.array([x_lim, x_lim-9 , x_lim-9, x_lim])
+    y_sq = np.array([3.2, 3.2, -3.2, -3.2])
+    plt.plot(x_sq, y_sq, 'k-', -x_sq, y_sq, 'k-')
+
+    # Plot goal line
+    goal_linex = np.array([x_lim, x_lim])
+    goal_liney = np.array([-9.6, 9.6])
+
+    # Plot posts
+    goal_post_x = x_sq[[0,3]]
+    goal_post_y = y_sq[[0,3]]
+
+    in_bounds = False
+
+    # Find ellipse dimensions to have a straight goal line
+    while not in_bounds:
+        x_lim += 0.01; y_lim += 0.01; 
+        rad_cc = np.array(((-goal_linex)**2/(x_lim)**2) + ((-goal_liney)**2/(y_lim)**2))
+        in_bounds = all(rad_cc < 1)
+
+    x = x_lim*np.cos(t)
+    y = y_lim*np.sin(t)
+    plt.plot(x[(x<goal_linex[0]) & (x>-goal_linex[0])], y[(x<goal_linex[0]) & (x>-goal_linex[0])], 'k-')
+
     # Generate 50m lines
     x = np.arange(0, 50.1, 0.1)
     y = np.sqrt(50**2 - x**2)
     x = np.concatenate([x,x[::-1]]) - x_lim
     y = np.concatenate([y,-y[::-1]])
 
-    # Clip 50m lines at boundary
-    cos_angle = np.cos(np.radians(180.))
-    sin_angle = np.sin(np.radians(180.))
-    xct = x * cos_angle - y * sin_angle
-    yct = x * sin_angle + y * cos_angle
-    rad_cc = np.array((xct**2/(x_lim)**2) + (yct**2/(y_lim)**2))
+    rad_cc = np.array(((-x)**2/(x_lim)**2) + ((-y)**2/(y_lim)**2))
     clip_circle = rad_cc < 1
-    y = y[clip_circle]
-    x = x[clip_circle]
 
-    # Plot 50m lines
-    plt.plot(x, y,'k-', -x, y, 'k-')
+    y = y[clip_circle]; x = x[clip_circle]
+    plt.plot(x, y, 'k-', -x, y, 'k-')
 
-    # Plot goal square
-    x_sq = np.array([x_lim, x_lim-9 , x_lim-9, x_lim])
-    y_sq = np.array([6.4, 6.4, -6.4, -6.4])
-    plt.plot(x_sq, y_sq,'k-', -x_sq, y_sq, 'k-')
-
-    # Plot centre square
+    plt.plot(goal_linex, goal_liney, 'k-', -goal_linex, goal_liney, 'k-')
+    plt.plot(goal_post_x, goal_post_y, 'ro', -goal_post_x, goal_post_y, 'ro', markersize=3)
+    plt.plot(goal_post_x, goal_post_y + [6.4, -6.4], 'bo', -goal_post_x, goal_post_y + [6.4, -6.4], 'bo',markersize=3)
+    # Centre square
     plt.plot([-25, -25, 25, 25, -25], [-25, 25, 25, -25, -25], 'k-')
 
-    # Plot centre point
     plt.scatter(0,0,s=15)
 
     plt.show()
